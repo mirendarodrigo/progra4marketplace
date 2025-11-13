5e46ef6e732835278f1116333d55e8493884f566
mirendarodigo <mirendarodrigo@gmail.com>
2025-11-13 15:27:03 -0300

    actualizacion build.sh

--- myclase/build.sh
+++ myclase/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 python -m pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+pip install -r requirements.txt;
+python manage.py collectstatic --no-input;
+python manage.py migrate; 
+python manage.py createsuperuser --noinput || true
