69309a5267cb1f7f988d4e6c9e4ebea373c2f2fd
mirendarodigo <mirendarodrigo@gmail.com>
2025-11-24 16:20:07 -0300

    rastreando statics

--- myclase/build.sh
+++ myclase/build.sh
@@ -8,16 +8,15 @@ pip install -r requirements.txt
 # Recolectar estáticos
 python manage.py collectstatic --no-input
 
-# --- DEPURACIÓN: LA HORA DE LA VERDAD ---
-echo "1. ¿Dónde estoy parado?"
-pwd
+# --- DEPURACIÓN: BÚSQUEDA IMPLACABLE ---
+echo "---- ¿DÓNDE ESTÁ WALLY? (styles.css) ----"
 
-echo "2. ¿Qué carpetas hay aquí en la raíz?"
-ls -F
+# Buscamos 'styles.css' en CUALQUIER carpeta desde aquí
+find . -name "styles.css"
 
-echo "3. Vamos a mirar dentro de la ruta que salió en los logs:"
-# Usamos la ruta que vimos en tu log anterior
-ls -R myclase/staticfiles || echo "⚠️ No se pudo acceder a myclase/staticfiles"
+echo "---- ¿DÓNDE ESTÁ LA CARPETA STATICFILES? ----"
+# Buscamos cualquier carpeta que se llame 'staticfiles'
+find . -type d -name "staticfiles"
 # ----------------------------------------
 
 python manage.py migrate
