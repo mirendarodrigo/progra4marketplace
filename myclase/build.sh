fbffb463492c92210b3678ae3a4399c0c892162b
mirendarodigo <mirendarodrigo@gmail.com>
2025-11-24 16:14:37 -0300

    build

--- myclase/build.sh
+++ myclase/build.sh
@@ -8,5 +8,16 @@ pip install -r requirements.txt
 # Recolectar estáticos
 python manage.py collectstatic --no-input
 
-# Migraciones
+# --- DEPURACIÓN: LA HORA DE LA VERDAD ---
+echo "1. ¿Dónde estoy parado?"
+pwd
+
+echo "2. ¿Qué carpetas hay aquí en la raíz?"
+ls -F
+
+echo "3. Vamos a mirar dentro de la ruta que salió en los logs:"
+# Usamos la ruta que vimos en tu log anterior
+ls -R myclase/staticfiles || echo "⚠️ No se pudo acceder a myclase/staticfiles"
+# ----------------------------------------
+
 python manage.py migrate
