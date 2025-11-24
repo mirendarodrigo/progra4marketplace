0359b2ee5c4ecd285934c1594f4fd71a1f51d5c6
mirendarodigo <mirendarodrigo@gmail.com>
2025-11-24 16:26:15 -0300

    Forzando collectsatic con clear

--- myclase/build.sh
+++ myclase/build.sh
@@ -5,18 +5,10 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Recolectar estáticos
-python manage.py collectstatic --no-input
+# --- EL CAMBIO CLAVE ---
+# --clear: Borra la carpeta de destino y fuerza la copia desde cero
+# --no-input: No preguntes "estás seguro?"
+python manage.py collectstatic --no-input --clear
 
-# --- DEPURACIÓN: BÚSQUEDA IMPLACABLE ---
-echo "---- ¿DÓNDE ESTÁ WALLY? (styles.css) ----"
-
-# Buscamos 'styles.css' en CUALQUIER carpeta desde aquí
-find . -name "styles.css"
-
-echo "---- ¿DÓNDE ESTÁ LA CARPETA STATICFILES? ----"
-# Buscamos cualquier carpeta que se llame 'staticfiles'
-find . -type d -name "staticfiles"
-# ----------------------------------------
-
+# Migraciones
 python manage.py migrate
